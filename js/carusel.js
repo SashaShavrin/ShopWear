cb55b87ff065b9b2436d44ae416191b15a50fbbd
Sasha <97390534+SashaShavrin@users.noreply.github.com>
2022-11-04 14:27:28 +0300

    Update carusel.js

--- js/carusel.js
+++ js/carusel.js
@@ -17,3 +17,21 @@ products.addEventListener('mouseover',
     }
   }
 );
+products.addEventListener('click',
+  event => {
+    let elem = event.target;
+    let elemParent = elem.parentNode.parentNode;
+    let front = elemParent.children[0];
+    let back = elemParent.children[1];
+    let classElem = event.target.className;
+    if (elem.classList.contains("point-front")) {
+      front.style.display = 'block';
+      back.style.display = 'none';
+    }
+    if (elem.classList.contains("point-back")) {
+      front.style.display = 'none';
+      back.style.display = 'block';
+      back.style.marginTop = '-0.5px'
+    }
+  }
+);
